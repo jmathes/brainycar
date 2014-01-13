d00bb237d5ad9614cc6c4a296a84a0eef2b06dd7
Joe Mathes <jmathes@gmail.com>
2014-01-13 12:39:10 -0800

    when climate settings are on, open/close sunroof when it will help achieve goal temp

--- brainycar.py
+++ brainycar.py
@@ -22,9 +22,23 @@ def run_all_checks(car):
     logging.info("awake: %s", car.awake)
     if car.awake:
         logging.info("speed: %s", car.drive_state['speed'])
-        if car.drive_state['speed'] >= 45:
-            car.sun_roof_control('close')
-
+        current_sunroof_state = car.vehicle_state['sun_roof_percent_open']
+        desired_sunroof_state = current_sunroof_state
+        if car.drive_state['speed'] >= 45 or car.drive_tate['speed'] is None:
+            desired_sunroof_state = 0
+        elif car.climate_state['is_auto_conditioning_on']:
+            goal = car.climate_state['driver_temp_setting']
+            outide = car.climate_state['outside_temp']
+            inside = car.climate_state['inside_temp']
+            if (True
+                    or goal < inside and outside < inside
+                    or goal > inside and outside > inside
+                    ):
+                desired_sunroof_state = 100
+            else:
+                desired_sunroof_state = 0
+        if desired_sunroof_state != current_sunroof_state:
+            car.sun_roof_control('open' if desired_sunroof_state == 100 else 'close')
 
 if __name__ == "__main__":
     car = tesla.my_car()
