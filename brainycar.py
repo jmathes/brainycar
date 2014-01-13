7996f69ad55f78da66b9aaf8734b825b412595bd
Joe Mathes <jmathes@gmail.com>
2014-01-13 00:37:56 -0800

    close the sunroof when speed is over 35

---
+++ brainycar.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import os
+from tesla import tesla
+from pprint import pprint
+
+grace = tesla.my_car()
+print grace.asleep
+pprint(grace.diagnostic())
+
+
+def main(car):
+    while True:
+        time.sleep(1)
+        try:
+            run_all_checks(car)
+        except:
+            pass
+
+def run_all_checks():
+    if not car.asleep:
+        if car.drive_state['speed'] > 35:
+            car.sun_roof_control('close')
+
+
+if __name__ == "__main__":
+    car = tesla.tesla.my_car()
+    main()
