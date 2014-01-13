e5ad29b07a48947accb412dcbc237d61fd6afe10
Joe Mathes <jmathes@gmail.com>
2014-01-13 08:33:47 -0800

    loggin

--- brainycar.py
+++ brainycar.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python
 import os
 import time
+import logging
 from tesla import tesla
 from pprint import pprint
 
+logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.DEBUG)
 
 def main(car):
     while True:
         time.sleep(1)
         try:
+            logging.info("checking %s", car)
             run_all_checks(car)
         except:
             pass
 
-def run_all_checks():
-    if not car.asleep:
+def run_all_checks(car):
+    logging.info("awake: %s", car.awake)
+    if car.awake:
+        logging.info("speed: %s", car.drive_state['speed'])
         if car.drive_state['speed'] > 45:
             car.sun_roof_control('close')
 
