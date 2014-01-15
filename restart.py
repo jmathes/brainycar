288675287b25514560ca9400b33875ad531c8c27
Joe Mathes <jmathes@gmail.com>
2014-01-15 14:25:37 -0800

    blah

--- restart.py
+++ restart.py
@@ -4,19 +4,11 @@ import time
 
 with open('pidfile', 'rb') as pidfile:
     pid = pidfile.readline()
-    time.sleep(1)
     print "killing %s" % pid
-    time.sleep(1)
     os.system("kill %s" % pid)
 
-time.sleep(1)
 print os.system("git pull")
-time.sleep(1)
 print os.system("git submodule update --recursive tesla")
-time.sleep(1)
 print os.system("nohup ./brainycar.py &")
-time.sleep(1)
 os.system("ps aux | grep brainycar | grep -v grep | grep -v restart")
-time.sleep(1)
 print "done"
-time.sleep(1)
